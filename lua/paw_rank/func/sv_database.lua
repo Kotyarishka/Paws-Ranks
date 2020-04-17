a2d247fe1e5b4105089258d97e2d3a0d50a70e48
Kotyarishka <44447172+Kotyarishka@users.noreply.github.com>
2020-04-17 22:56:51 +0300

    database-setup

---
+++ lua/paw_rank/func/sv_database.lua
@@ -0,0 +1,27 @@
+/*
+
+Database functions for Paws Module 'Rank'
+
+Made by Kot from "🐾 Aw... Paws!"
+
+*/
+
+local MODULE = Paws.Lib.Module('rank')
+MODULE.Rank = MODULE.Rank or {}
+MODULE.Config = MODULE.Config or {}
+
+MODULE.Database = {}
+
+/*
+    Database init function
+*/
+
+function MODULE.Database:Init()
+    local path = 'paws/'..MODULE.UID..'/'
+
+    if !file.Exists(path, 'DATA') then
+        file.CreateDir(path)
+    end
+
+    return true
+end
